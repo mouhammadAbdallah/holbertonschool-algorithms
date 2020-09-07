bcdfe2ceda24ec5ac6e2bc15904c6978dbd9d323
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-07 12:38:17 +0300

    task 100

---
+++ 0x00-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - before main
+ *
+ * void
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
