d39f825ce0417fb81840f00dad28d0f5d1f8b2ea
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-08 17:47:47 +0300

    task 8

---
+++ 0x01-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum_listint
+ * @head: head
+ *
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int s = 0;
+
+	if (head == NULL)
+		return (0);
+	do
+	{
+		s += head->n;
+		head = head->next;
+	} while (head != NULL);
+	return (s);
+}
