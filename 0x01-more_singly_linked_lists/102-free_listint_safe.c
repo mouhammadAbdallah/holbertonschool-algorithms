fb98c91daf519af3607e608cae41168bba3ef944
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-08 20:15:52 +0300

    task 102

---
+++ 0x01-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,68 @@
+#include "lists.h"
+
+/**
+ * floyds_cycle2 - checks for infinite loops.
+ * @head: Linked list.
+ * Return: Loop length.
+ */
+int floyds_cycle2(const listint_t *head)
+{
+	const listint_t *slow_p, *fast_p;
+	int nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	slow_p = head->next;
+	fast_p = (head->next)->next;
+	while (fast_p)
+	{
+		if (slow_p == fast_p)
+		{
+			slow_p = head;
+			for (; slow_p != fast_p; nodes++)
+			{
+				slow_p = slow_p->next;
+				fast_p = fast_p->next;
+			}
+			slow_p = slow_p->next;
+			for (; slow_p != fast_p; nodes++)
+				slow_p = slow_p->next;
+			return (nodes);
+		}
+		slow_p = slow_p->next;
+		fast_p = (fast_p->next)->next;
+	}
+
+	return (0);
+}
+/**
+ * free_listint_safe - Free linked list.
+ * @h: Linked list.
+ * Return: Number of nodes in list.
+ */
+int free_listint_safe(listint_t **h)
+{
+	listint_t *aux;
+	int nodes = floyds_cycle2(*h), id;
+
+	if (nodes == 0)
+	{
+		for (nodes = 0; h && *h; nodes++)
+		{
+			aux = (*h)->next;
+			free(*h);
+			*h = aux;
+		}
+		h = NULL;
+		return (nodes);
+	}
+	for (id = 0; id < nodes; id++)
+	{
+		aux = (*h)->next;
+		free(*h);
+		*h = aux;
+	}
+	*h = NULL;
+	h = NULL;
+	return (nodes);
+}
