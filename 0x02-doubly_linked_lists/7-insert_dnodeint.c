7ee15c260e63a39e30882f4515aa8aad5507bef1
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-15 20:59:21 +0300

    task 7

--- 0x02-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x02-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,29 +10,38 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int id;
-	dlistint_t *current = *h;
-	dlistint_t *new;
+	dlistint_t *ptr, *new;
+	unsigned int i = 0;
 
-	if (*h == NULL && idx > 0)
+	if (h == NULL)
 		return (NULL);
 	if (idx == 0)
-		return (add_dnodeint(&h, n));
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	new->prev = NULL;
-	current = *h;
-	for (id = 0; id < idx - 1; id++)
 	{
-		current = current->next;
-		if (current == NULL && idx - id > 0)
-			return (free(new), NULL);
+		ptr = add_dnodeint(h, n);
+		return (ptr);
 	}
-	new->next = current->next;
-	new->prev = current;
-	current->next = new;
-	return (new);
+	ptr = *h;
+	while (ptr != NULL && i < idx)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	if (ptr != NULL)
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		ptr->prev->next = new;
+		new->prev = ptr->prev;
+		new->next = ptr;
+		ptr->prev = new;
+		return (new);
+	}
+	else if (i == idx)
+	{
+		ptr = add_dnodeint_end(h, n);
+		return (ptr);
+	}
+	return (NULL);
 }
