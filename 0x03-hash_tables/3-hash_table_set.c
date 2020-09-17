dfb00065ea79483193e7374141d1d455e7b03683
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-17 13:29:59 +0300

    task 3

--- 0x03-hash_tables/3-hash_table_set.c
+++ 0x03-hash_tables/3-hash_table_set.c
@@ -13,13 +13,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (ht == NULL)
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 	node->key = strdup(key);
 	node->value = strdup(value);
-	node->next = NULL;
 	node->next = (ht->array)[index];
 	(ht->array)[index] = node;
 	return (1);
