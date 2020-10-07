7fce6fc0baba8791cbdea73fbd609cd937f441ed
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-10-07 18:47:34 +0200

    task 0

---
+++ 0x04-graphs/0-graph_create.c
@@ -0,0 +1,18 @@
+#include "graphs.h"
+
+/**
+ * graph_create - create
+ *
+ * Return: graph_t
+ */
+graph_t *graph_create(void)
+{
+	graph_t *graph;
+
+	graph = malloc(sizeof(graph_t));
+	if (!graph)
+		return (NULL);
+	graph->nb_vertices = 0;
+	graph->vertices = NULL;
+	return (graph);
+}
