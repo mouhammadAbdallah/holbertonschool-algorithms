469908ca0aa0d6e5120a4f82d7723111b6399068
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-10-07 19:22:06 +0200

    task 3 4

--- 0x04-graphs/graphs.h
+++ 0x04-graphs/graphs.h
@@ -70,5 +70,8 @@ typedef struct graph_s
 void graph_display(const graph_t *graph);
 graph_t *graph_create(void);
 vertex_t *graph_add_vertex(graph_t *graph, const char *str);
+int graph_add_edge(graph_t *graph, const char *src, const char *dest,
+		   edge_type_t type);
+void graph_delete(graph_t *graph);
 
 #endif /* GRAPHS_H */
